7df9df371cb46621eda1891986ed95b4570137d6
Shayan <nwryshayan500@gmail.com>
2025-03-17 20:28:19 +0330

    009 Adjusting Server Actions for useFormState

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,43 +1,11 @@
 'use client'
 
 import { useFormState } from 'react-dom';
-import { redirect } from 'next/navigation';
 
-import { storePost } from '@/lib/posts';
 import FormSubmitBtn from '@/components/AddPost/FormSubmitBtn';
+import { createPost } from '@/lib/actions';
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    'use server'
-    const title = formData.get('title')
-    const image = formData.get('image')
-    const content = formData.get('content')
-
-    let errors = {}
-
-    if(!title || title.trim().length === 0) {
-      errors.title = "Title is required"
-    }
-    if(!content || content.trim().length === 0) {
-      errors.content = "Content is required"
-    }
-    if(!image || image.size === 0) {
-      errors.image = "Image is required"
-    }
-
-    if(Object.keys(errors).length > 0) return errors
-
-    const post = {
-      imageUrl: 'https://www.slashgear.com/img/gallery/10-old-school-lexus-models-that-are-still-affordable-for-now/lexus-is-300-1730087299.webp',
-      title,
-      content,
-      userId: 1
-    }
-    await storePost(post)
-
-    redirect('/feed')
-  }
-
   const [state, formAction] = useFormState(createPost, {})
 
   return (
@@ -46,7 +14,8 @@ export default function NewPostPage() {
       <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" required />
+          <input type="text" id="title" name="title" />
+          {state.title && <p className='form-errors'>{state.title}</p>}
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
@@ -55,12 +24,13 @@ export default function NewPostPage() {
             accept="image/png, image/jpeg"
             id="image"
             name="image"
-            required
           />
+          {state.image && <p className='form-errors'>{state.image}</p>}
         </p>
         <p className="form-control">
           <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" required/>
+          <textarea id="content" name="content" rows="5" />
+          {state.content && <p className='form-errors'>{state.content}</p>}
         </p>
         <p className="form-actions">
           <FormSubmitBtn />
