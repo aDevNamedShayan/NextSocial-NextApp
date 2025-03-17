55b2062c034756579133386d292d399b153c6891
Shayan <nwryshayan500@gmail.com>
2025-03-17 12:34:50 +0330

    002 Starting Project & Analyzing Mutation Options

---
+++ app/api/posts/route.js
@@ -0,0 +1,7 @@
+// POST /api/posts
+
+export default function POST(request) {
+  // ...
+}
+
+// Works great if we have another client which should also submit user data to our database, for instance a mobile app
